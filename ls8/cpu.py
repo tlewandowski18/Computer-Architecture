a535aecbf54f97e2a992d1e568b0bb238ce6730a
tlewandowski18 <tlewandowski18@gmail.com>
2020-09-13 18:27:42 -0400

    Complete stack functions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,8 +9,9 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[7] = 0xf4
         self.pc = 0
-
+       
     def ram_read(self, mar):
         return self.ram[mar]
 
@@ -90,9 +91,31 @@ class CPU:
                 reg_a = self.ram[self.pc + 1]
                 reg_b = self.ram[self.pc + 2]
                 self.alu("MULT", reg_a, reg_b)
+            #PUSH Instruction Handler
+            elif ir == 0b01000101:
+                #decrement the stack pointer
+                self.reg[7] -= 1
+                #pull reg index from memory
+                reg_num = self.ram[self.pc + 1]
+                #pull value from reg
+                value = self.reg[reg_num]
+                #Store at top of stack
+                top_of_stack_addr = self.reg[7]
+                self.ram[top_of_stack_addr] = value
+            #POP INSTRUCTION Handler
+            elif ir == 0b01000110:
+                #find stop of stack and copy value
+                top_of_stack_addr = self.reg[7]
+                value = self.ram[top_of_stack_addr]
+                #find reg spot and store the value at that place in reg
+                reg_num = self.ram[self.pc + 1]
+                self.reg[reg_num] = value
+                #increment stack pointer
+                self.reg[7] += 1
             #HLT Instruction Handler
             elif ir == 1:
                 running = False
+
             num_operands = ir >> 6
             increment = num_operands + 1
             self.pc += increment
